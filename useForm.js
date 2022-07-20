9b2c1733b9072682d71a8e8200da3bde0cf3888a
Paco Valero <fvalero@pavagua.com>
2022-07-20 13:50:01 +0200

    Primer commit

---
+++ useForm.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export const useForm = ( initialForm = {} ) => {
+  
+    const [formState, setFormState] = useState( initialForm );
+
+    // const { username, email, password } = formState;
+
+    const onInputChange = ({ target }) => {
+        const { name, value } = target;
+        setFormState({
+            ...formState,
+            [name]: value
+        });
+    }
+
+    const onResetForm = () => {
+        // setFormState({
+        //     ...initialForm
+        // });
+        setFormState(initialForm);
+    }
+
+    return {
+        ...formState,
+        formState,
+        onInputChange,
+        onResetForm
+    }
+}
